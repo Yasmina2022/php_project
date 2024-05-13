55c274f77a71c07461bfecb06db6579a7b3f0027
Yasmina2022 <yasminfarahat5@gmail.com>
2024-05-13 08:15:16 -0700

    Add files via upload

---
+++ first_project_php.php
@@ -0,0 +1,12 @@
+<?php
+echo "my first task in php<br>";
+$first_name = "Yasmin";          //string
+
+$number1 = 50;                     //integer
+$number2 = true;                   //boolean
+echo "<br>";
+
+
+echo  "$first_name<br>" ,"THE FIRST VARIABLE IS : $number1<br>" , "THE SECOND VARIABLE IS : $number2<br>";
+
+?>
---
+++ new_php.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "hello world";
+
+
+$first_name = "yasmin";
+$number1 = 20;
+$number2 = 30;
+
+
+echo "<h1>hello $first_name</h1>";
+echo "hello world!<br>";
+echo "iam about to learn php!<br>";
+
+$test1 = "A";                                        //string
+$test2 = false;                                       //boolean
+
+
+var_dump($test1);
+echo "<br>";
+var_dump($test2);
+
+
+?> 
